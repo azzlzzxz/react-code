e1105fa5975d0532458688308116d1450005bbfb
azzlzzxz <37786423+azzlzzxz@users.noreply.github.com>
2024-09-23 00:37:31 +0800

    feat: multiNode

--- src/main.jsx
+++ src/main.jsx
@@ -1,50 +1,9 @@
 import * as React from "./react";
 import { createRoot } from "react-dom/src/client/ReactDOMRoot";
 
-/** 单节点 key相同 type 相同 */
+/** 第一次循环跳出例子 */
 function FunctionComponent1() {
-  const [number, setNumber] = React.useState(0);
-
-  return number === 0 ? (
-    <div key="title1" id="title1" onClick={() => setNumber(number + 1)}>
-      title
-    </div>
-  ) : (
-    <div key="title1" id="title2" onClick={() => setNumber(number + 1)}>
-      title2
-    </div>
-  );
-}
-
-/** 单节点 key不同 type 相同 */
-function FunctionComponent2() {
-  const [number, setNumber] = React.useState(0);
-  return number === 0 ? (
-    <div key={"title1"} onClick={() => setNumber(number + 1)} id="title">
-      title
-    </div>
-  ) : (
-    <div onClick={() => setNumber(number + 1)} key={"title2"} id="title2">
-      title2
-    </div>
-  );
-}
-
-/** 单节点 key相同 type 不同 */
-function FunctionComponent3() {
-  const [number, setNumber] = React.useState(0);
-  return number === 0 ? (
-    <div onClick={() => setNumber(number + 1)} key="title1" id="title1">
-      title1
-    </div>
-  ) : (
-    <p onClick={() => setNumber(number + 1)} key="title1" id="title1">
-      title1
-    </p>
-  );
-}
-
-function FunctionComponent() {
+  console.log("FunctionComponent");
   const [number, setNumber] = React.useState(0);
   return number === 0 ? (
     <ul key="container" onClick={() => setNumber(number + 1)}>
@@ -52,13 +11,48 @@ function FunctionComponent() {
       <li key="B" id="B">
         B
       </li>
-      <li key="C">C</li>
+      <li key="C" id="C">
+        C
+      </li>
     </ul>
   ) : (
     <ul key="container" onClick={() => setNumber(number + 1)}>
-      <li key="B" id="B2">
+      <li key="A">A2</li>
+      <p key="B" id="B2">
+        B2
+      </p>
+      <li key="C" id="C2">
+        C2
+      </li>
+    </ul>
+  );
+}
+
+function FunctionComponent() {
+  console.log("FunctionComponent");
+  const [number, setNumber] = React.useState(0);
+
+  return number === 0 ? (
+    <ul key="container" onClick={() => setNumber(number + 1)}>
+      <li key="A">A</li>
+      <li key="B" id="b">
+        B
+      </li>
+      <li key="C">C</li>
+      <li key="D">D</li>
+      <li key="E">E</li>
+      <li key="F">F</li>
+    </ul>
+  ) : (
+    <ul key="container" onClick={() => setNumber(number + 1)}>
+      <li key="A">A2</li>
+      <li key="C">C2</li>
+      <li key="E">E2</li>
+      <li key="B" id="b2">
         B2
       </li>
+      <li key="G">G</li>
+      <li key="D">D2</li>
     </ul>
   );
 }
